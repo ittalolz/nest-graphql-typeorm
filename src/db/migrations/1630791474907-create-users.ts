82721a5310e39f51311cdf64a1f430884f152a86
Ittalo Lima Steltter <ittalolz@gmail.com>
2021-09-06 08:14:31 -0400

    inicial

---
+++ src/db/migrations/1630791474907-create-users.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsers1630791474907 implements MigrationInterface {
+  private table = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true, // Auto-increment
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        length: '255',
+        isUnique: true,
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(true);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
