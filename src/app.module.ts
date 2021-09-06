126408f63364b6cec255d81fa203c812136f0f1c
Ittalo Lima Steltter <ittalolz@gmail.com>
2021-09-06 14:50:39 -0400

    input e resolvers user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { AppService } from './app.service';
 import * as TypeORMOptions from './config/orm';
 import RepoModule from './repo.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import UserResolver from './resolvers/user.resolver';
+
+const gqlImports = [UserResolver];
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeORMOptions),
     RepoModule,
+    ... gqlImports,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       playground: true,
